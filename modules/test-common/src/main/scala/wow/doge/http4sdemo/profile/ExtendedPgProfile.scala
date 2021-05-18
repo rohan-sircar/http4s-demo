1e68e8a86311e7ff02c4b7a99c54c739fd3003b9
Rohan Sircar <rohansircar@tuta.io>
2021-05-18 19:55:37 +0530

    Left out committing pg profile changes

--- modules/test-common/src/main/scala/wow/doge/http4sdemo/profile/ExtendedPgProfile.scala
+++ modules/test-common/src/main/scala/wow/doge/http4sdemo/profile/ExtendedPgProfile.scala
@@ -3,13 +3,24 @@ package wow.doge.http4sdemo.profile
 import be.venneborg.refined.RefinedMapping
 import be.venneborg.refined.RefinedSupport
 import slick.jdbc.PostgresProfile
+import com.github.tminglei.slickpg._
+import enumeratum.SlickEnumSupport
+import com.github.tminglei.slickpg.str.PgStringSupport
 
 trait ExtendedPgProfile
     extends PostgresProfile
     with RefinedMapping
-    with RefinedSupport {
+    with RefinedSupport
+    with PgCirceJsonSupport
+    with array.PgArrayJdbcTypes
+    with PgStringSupport
+    with SlickEnumSupport {
+  override val pgjson = "jsonb"
 
-  override val api = new API with RefinedImplicits
+  override val api = new API
+    with RefinedImplicits
+    with JsonImplicits
+    with PgStringImplicits
 
 }
 
